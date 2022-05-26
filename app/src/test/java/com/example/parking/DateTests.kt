fa6475927ca77a92618e5d409944dc1dfd46f383
ISMashtakov <ismashtakov@edu.hse.ru>
2022-05-26 22:20:07 +0300

    add one test

---
+++ app/src/test/java/com/example/parking/DateTests.kt
@@ -0,0 +1,28 @@
+package com.example.parking
+
+import com.example.parking.utils.toDate
+import org.junit.Test
+import java.text.SimpleDateFormat
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.test.assertEquals
+import kotlin.time.Duration.Companion.minutes
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class DateTests {
+    @Test
+    fun localDateTimeConvertToDateCorrected() {
+        val localDateTime = LocalDateTime.of(2022, 4, 20, 12, 32)
+        val date = localDateTime.toDate()
+
+        val formatString = "yyyy-MM-dd'T'HH:mm:ss.S"
+        val simpleDateFormat = SimpleDateFormat(formatString)
+        val simpleLocalDateFormat = DateTimeFormatter.ofPattern(formatString)
+        assertEquals(simpleLocalDateFormat.format(localDateTime), simpleDateFormat.format(date))
+    }
+}
